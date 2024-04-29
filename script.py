0bcb27de57accaa35a817d899ddae8decd72548c
rodrigo-navegantes <navegantesmalato@gmail.com>
2024-04-28 21:45:25 -0300

    My second commit

---
+++ script.py
@@ -0,0 +1,27 @@
+import os
+import shutil
+
+dd_dir = "/home/rodrigo/Downloads"
+image_dir = "/home/rodrigo/Pictures"
+document_dir = "/home/rodrigo/Documents"
+del_dir = "/home/rodrigo/Delete"
+
+files = os.listdir(dd_dir)
+
+for file in files:
+    file_path = os.path.join(dd_dir, file)
+    
+    #Check if file is an image
+    if file.lower().endswith(('.png', '.jpg', '.jpeg', '.gif', '.bmp', '.mp3', '.mp4')):
+        shutil.move(file_path, image_dir)
+        print("Moved {file} to {image_dir}")
+    
+    # Check if file is a document
+    elif file.lower().endswith(('.pdf', '.doc', '.docx', '.txt')):
+        shutil.move(file_path, document_dir)
+        print("Moved {file} to {document_dir}")
+    
+    # Otherwise, move it to the 'delete' directory
+    else:
+        shutil.move(file_path, del_dir)
+        print("Moved {file} to {other_dir}")
